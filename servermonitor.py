1e0002e032e0c940a37ef5f0ee6ee1917ac8926d
FadhelBoussen <144439317+FadhelBoussen@users.noreply.github.com>
2023-09-13 09:56:27 +0100

    Add files via upload

---
+++ servermonitor.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import requests
+import datetime 
+import ping3
+
+
+Date=datetime.datetime.now()
+print("date:  ",Date) 
+
+def get_website_status(url):
+  try:
+    response = requests.get(url)
+    return response.status_code
+  except requests.exceptions.RequestException as e:
+    pass
+    return None
+
+url = ["127.0.0.1"]   
+ 
+for i in url :
+    status_code = get_website_status(i)
+    if status_code == 200 :
+       print ("website ok :", i)
+    else :
+       print ("website nok :", i)
+
+
+
+hosts=["127.0.0.1","192.168.1.2"]
+
+for i in hosts:
+    rrt=ping3.ping(i)
+    if rrt is not None:
+       print("server is running:",i)
+    else:
+       print("server is down:",i)
