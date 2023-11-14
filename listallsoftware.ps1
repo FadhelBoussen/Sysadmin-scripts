0ad15596fa9231b75c367ca373565f103ca0b589
FadhelBoussen <144439317+FadhelBoussen@users.noreply.github.com>
2023-11-14 12:58:35 +0100

    Create listallsoftware.ps1

---
+++ listallsoftware.ps1
@@ -0,0 +1,16 @@
+    $installed1 = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
+        Select-Object DisplayName, Publisher, Version, InstallDate |
+        Sort-Object -Property InstallDate -Descending
+
+    $installed2 = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |
+        Select-Object DisplayName, Publisher, Version, InstallDate |
+        Sort-Object -Property InstallDate -Descending
+
+
+    $allInstalledPrograms = $installed1 + $installed2
+
+
+    $allInstalledPrograms | Format-Table -AutoSize
+
+
+    $allInstalledPrograms | Export-Csv -Path "C:\users\fadhel.boussen\documents\AllInstalledPrograms.csv" -NoTypeInformation
