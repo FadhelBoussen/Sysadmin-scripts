b736e185027cda9e4a18afacf767014f53d5604c
FadhelBoussen <144439317+FadhelBoussen@users.noreply.github.com>
2023-11-14 13:01:21 +0100

    listallsoftware.ps1

--- listallsoftware.ps1
+++ listallsoftware.ps1
@@ -1,16 +1,16 @@
-    $installed1 = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
-        Select-Object DisplayName, Publisher, Version, InstallDate |
-        Sort-Object -Property InstallDate -Descending
+$installed1 = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
+    Select-Object DisplayName, Publisher, Version, InstallDate |
+    Sort-Object -Property InstallDate -Descending
 
-    $installed2 = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |
-        Select-Object DisplayName, Publisher, Version, InstallDate |
-        Sort-Object -Property InstallDate -Descending
-
-
-    $allInstalledPrograms = $installed1 + $installed2
+$installed2 = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |
+    Select-Object DisplayName, Publisher, Version, InstallDate |
+    Sort-Object -Property InstallDate -Descending
 
 
-    $allInstalledPrograms | Format-Table -AutoSize
+$allInstalledPrograms = $installed1 + $installed2
 
 
-    $allInstalledPrograms | Export-Csv -Path "C:\users\fadhel.boussen\documents\AllInstalledPrograms.csv" -NoTypeInformation
+$allInstalledPrograms | Format-Table -AutoSize
+
+
+$allInstalledPrograms | Export-Csv -Path "C:\users\fadhel.boussen\documents\AllInstalledPrograms.csv" -NoTypeInformation
